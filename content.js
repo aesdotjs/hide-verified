64c1424fa2529e6acf138f80ecce78d99c01b6e4
aesdotjs <skriabin.simon@gmail.com>
2023-08-01 12:27:28 +0200

    whitelist

--- content.js
+++ content.js
@@ -6,6 +6,27 @@ window.onload = function() {
     hideVerifiedTweets();
   }, 1000);
 };
+let whitelist = {};
+
+chrome.storage.sync.get(['whitelist'], function(result) {
+  whitelist = result.whitelist || {};
+});
+
+function toggleWhitelistUser(username) {
+  // If the user is already in the whitelist, remove them
+  if (whitelist[username]) {
+    delete whitelist[username];
+  } else {
+    // If the user is not in the whitelist, add them
+    whitelist[username] = true;
+  }
+
+  // Update the whitelist in chrome.storage.sync
+  chrome.storage.sync.set({whitelist: whitelist}, function() {
+    // After the storage update has completed, reapply the tweet hiding
+    hideVerifiedTweets();
+  });
+}
 
 function hideVerifiedTweets() {
   // Find all verified icons
@@ -19,6 +40,15 @@ function hideVerifiedTweets() {
     }
     // Hide the tweet text if it's a tweet from a verified user
     if (tweetElement) {
+      const userNameElement = tweetElement.querySelector('[data-testid="User-Name"] a');
+      let username = null;
+      if(userNameElement) {
+        username = userNameElement.getAttribute('href').substring(1); // Remove the initial "/"
+        if(whitelist[username]) {
+          // If the user is in the whitelist, skip hiding the tweet
+          return;
+        }
+      }
       const tweetText = tweetElement.querySelector('[data-testid="tweetText"]');
       if (tweetText) {
         if (tweetText.dataset.unblur || tweetText.dataset.blurred) {
@@ -27,11 +57,32 @@ function hideVerifiedTweets() {
         // Create a blur mask for the tweet
         let blurMask = document.createElement("div");
         tweetText.dataset.blurred = true;
-        blurMask.innerHTML = "Cliquez pour afficher le tweet";
+        // blurMask.innerHTML = "Cliquez pour afficher le tweet";
+        // instead I want a div with the text and a div with a button to toggle the user in the whitelist
+        // append the divs to the blurMask
+        let blurMaskText = document.createElement("div");
+        blurMaskText.innerHTML = "Cliquez pour afficher le tweet";
+        let blurMaskButton = document.createElement("div");
+        blurMaskButton.innerHTML = "Ajouter à la whitelist";
+        blurMaskButton.style.padding = "5px";
+        blurMaskButton.style.border = "1px solid black";
+        blurMaskButton.style.borderRadius = "5px";
+        blurMaskButton.style.cursor = "pointer";
+        blurMaskButton.onclick = function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+          toggleWhitelistUser(username);
+          blurMask.style.display = 'none';
+          tweetText.dataset.unblur = true;
+          tweetText.style.filter = "none";
+        };
+        blurMask.appendChild(blurMaskText);
+        blurMask.appendChild(blurMaskButton);
         blurMask.style.position = "absolute";
         blurMask.style.textAlign = "center";
         blurMask.style.zIndex = "1000";
         blurMask.style.display = "flex";
+        blurMask.style.gap = "10px";
         blurMask.style.fontWeight = "bold";
         blurMask.style.justifyContent = "center";
         blurMask.style.alignItems = "center";
